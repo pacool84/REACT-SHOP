885f642c3f8083bd11df0f000ca4484690793943
Francisco López <flopez_1384@hotmail.com>
2022-04-24 16:04:13 -0500

    [ADD] The script to start DEV environment and also another to made the BUILD process

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <h1>Hello again my friend REACT</h1>;
+  return <h1>Hello again my friend REACT and all MOTHERFUCKERS!!!</h1>;
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"), //Dist es por convencion y significa distribution
     filename: "bundle.js",
   },
+  mode: "development",
   resolve: {
     extensions: [".js", ".jsx"],
   },
