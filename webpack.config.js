412df7a9ff1eb3ca94b5be345e639f39f4de92a5
Francisco López <flopez_1384@hotmail.com>
2022-04-24 15:39:01 -0500

    [ADD] BABEL and Webpack configuration, also add a missing plugin of html webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"), //Dist es por convencion y significa distribution
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, //Esto es un REGEX y se entiende todos los archivos js y jsx para identificarlos
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/, //Lee todos los archivos HTML
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
