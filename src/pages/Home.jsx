9389c0de273dba21b0d6c84201d9556f091a1498
Francisco López <flopez_1384@hotmail.com>
2022-04-27 22:36:10 -0500

    [ADD] The Header component to the Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-
+import Header from "../components/Header";
 const Home = () => {
-  return <h1>Hello my Friend Home</h1>;
+  return <Header />;
 };
 
 export default Home;
