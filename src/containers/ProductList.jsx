0263e8e02f9bf048e4c0e483765656964c941a84
Francisco López <flopez_1384@hotmail.com>
2022-05-03 22:29:06 -0500

    [ADD] useEffect to call API of the product list, babel plugin to use async/await and call the API, AXIOS to handle the calls for the API

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import ProductItem from "@components/ProductItem";
 import "@styles/ProductList.scss";
 
+const API = "https://api.escuelajs.co/api/v1/products";
+
 const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  /* Usando AXIOS para hacer el llamdo  */
+  useEffect(async () => {
+    const response = await axios(API);
+    setProducts(response.data);
+  }, []);
+
+  /* Usando metodo tradicional */
+  /*   const fetchProducts = async () => {
+    const response = await fetch(API);
+    const product = await response.json();
+    setProducts(product);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []); */
+
   return (
     <section className="main-container">
       <div className="ProductList">
+        {products.map((product) => (
+          <ProductItem />
+        ))}
         <ProductItem />
       </div>
     </section>
