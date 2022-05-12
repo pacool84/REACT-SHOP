d1f4a53ad79b85c88dae00fae01ccbe0df91c820
Francisco López <flopez_1384@hotmail.com>
2022-05-11 22:34:34 -0500

    [ADD] The logic to pass the props of the item in to the OrderItem component

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -3,14 +3,14 @@ import "@styles/OrderItem.scss";
 const imageItem =
   "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
 
-const OrderItem = () => {
+const OrderItem = ({ product }) => {
   return (
     <div className="OrderItem">
       <figure>
-        <img src={imageItem} alt="bike" />
+        <img src={product.images[0]} alt={product.title} />
       </figure>
-      <p>Bike</p>
-      <p>$30,00</p>
+      <p>{product.title}</p>
+      <p>${product.price}</p>
       <img src="./icons/icon_close.png" alt="close" />
     </div>
   );
